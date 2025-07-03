27f2149dd945baf523b8e745f1c91a0b0af29865
oshit.rul <oshit.dar.riseuplabs@gmail.com>
2025-07-03 16:24:39 +0600

    Refactor input handling and formatting in problem 1012 solution

--- beecrowd/1012.py
+++ beecrowd/1012.py
@@ -1,4 +1,4 @@
-# QUE 
+# QUE
 # Make a program that reads three floating point values: A, B and C. Then, calculate and show:
 # a) the area of the rectangled triangle that has base A and height C.
 # b) the area of the radius's circle C. (pi = 3.14159)
@@ -8,16 +8,20 @@
 # Input
 # The input file contains three double values with one digit after the decimal point.
 
-A = float(input())
-B = float(input())
-C = float(input())
+
+line = input()
+A, B, C = line.split()
+
+A = float(A)
+B = float(B)
+C = float(C)
 
 pi = 3.14159
 
 triangle = (A * C) / 2
-circle = pi * (C ** 2)
+circle = pi * (C**2)
 trapezium = ((A + B) / 2) * C
-square = B ** 2
+square = B**2
 rectangle = A * B
 
 print(f"TRIANGULO: {triangle:.3f}")
